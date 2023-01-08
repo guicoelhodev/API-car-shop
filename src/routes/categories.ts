160854cfae073284d65a35ab48b54b21fd2b02fe
guicoelhodev <gs.coelho@outlook.com>
2023-01-08 16:44:09 -0300

    refactor: remove controllers and add service class

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,8 +1,29 @@
-import express from "express";
-import { createCategories } from "../controllers/categories";
+import express, { Request, Response } from "express";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+// services
+
+import { CreateCategoryService } from "../services/CreateCategoryService";
+import { ListCategoryService } from "../services/ListCategoryService";
 
 const categoriesRoutes = express.Router();
 
-categoriesRoutes.post("/", createCategories);
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request: Request, response: Response) => {
+  const { name, description } = request.body;
+
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
+
+  return response.status(201).json({ message: "category has been created" });
+});
+
+categoriesRoutes.get("/", (request: Request, response: Response) => {
+  const listCategoryService = new ListCategoryService(categoriesRepository);
+  const allData = listCategoryService.execute();
+
+  return response.status(200).json({ data: allData });
+});
 
 export { categoriesRoutes };
