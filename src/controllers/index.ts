89db488b7e2e71efdd821ee82dd6155793160712
guicoelhodev <gs.coelho@outlook.com>
2023-01-08 13:24:11 -0300

    feat: move function that enable to createCategories to use  into controllers directory

--- src/controllers/index.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-export const helloWorld = (request: Request, response: Response) => {
-  
-  return response.status(200).json({ message: 'Hello new app!!'})
-}
--- src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import { helloWorld } from '../controllers';
-
-export const router = express.Router();
-
-router.get('/', helloWorld)
