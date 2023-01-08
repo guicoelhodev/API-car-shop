28b4cde829df9603197b8e1e9c049b31a45b95ee
guicoelhodev <gs.coelho@outlook.com>
2023-01-08 16:43:53 -0300

    feat: create ListCategory service and CreateCategory service

---
+++ src/services/CreateCategoryService/index.ts
@@ -0,0 +1,22 @@
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepositories: ICategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepositories.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists");
+    }
+
+    this.categoriesRepositories.createCategory({ name, description });
+  }
+}
+
+export { CreateCategoryService };
