b1c0faa5539070c7abaa244f204863cd12322a24
guicoelhodev <gs.coelho@outlook.com>
2023-01-08 13:23:41 -0300

    refactor: add default path to use into categories route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import express from 'express';
-import { router } from './routes';
-
+import express from "express";
+import { categoriesRoutes } from "./routes/categories";
 const app = express();
 
 const port = 8080;
 
-app.use(express.json())
-app.use(router);
+app.use(express.json());
+app.use("/categories", categoriesRoutes);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
