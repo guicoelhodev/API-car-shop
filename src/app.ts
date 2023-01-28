1a9a341935934ff2afd42088a089c95ad90c07c1
guicoelhodev <gs.coelho_dev@outlook.com>
2023-01-28 17:53:12 -0300

    feat: add new routes, specifications

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { categoriesRoutes } from "./routes/categories";
+import { specificationsRoutes } from "./routes/specifications";
 const app = express();
 
 const port = 8080;
 
 app.use(express.json());
+
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
