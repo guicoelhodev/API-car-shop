bf6336980e21bb7a9a0464804a8ae0dd9683c75e
guicoelhodev <gs.coelho_dev@outlook.com>
2023-01-28 17:54:20 -0300

    feat: create SpecificationsRepository

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,34 @@
+import { Specifications } from "../model/Specifications";
+import {
+  ICreateSpecificationsDTO,
+  ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specifications[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ description, name }: ICreateSpecificationsDTO): void {
+    const specification = new Specifications();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specifications | null {
+    const specification = this.specifications.find(
+      (specification) => specification.name == name
+    );
+    return specification ? specification : null;
+  }
+}
+
+export { SpecificationsRepository };
