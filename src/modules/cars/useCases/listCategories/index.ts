da95f5c5c823deca52616e6abcbfb615b38bcd8c
guicoelhodev <gs.coelho_dev@outlook.com>
2023-01-28 19:59:39 -0300

    refactor: create index export file to instance all class needed to execute listCategories action

---
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -0,0 +1,13 @@
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { ListCategoriesController } from "./ListCategoriesController";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+const categoriesRepositories = CategoriesRepository.getInstance();
+
+const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepositories);
+
+const listCategoriesController = new ListCategoriesController(
+  listCategoriesUseCase
+);
+
+export { listCategoriesController };
