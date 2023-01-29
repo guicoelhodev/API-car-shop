db66853f8fc8419fb24f5e3dbce116a1674a188f
guicoelhodev <gs.coelho_dev@outlook.com>
2023-01-29 14:15:23 -0300

    refactor: create index export route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories";
-import { specificationsRoutes } from "./routes/specifications";
+import { router } from "./routes";
 const app = express();
 
 const port = 8080;
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
