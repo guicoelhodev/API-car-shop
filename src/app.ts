7c48f81a8a887f509529372e2f01d556ef2b27a5
guicoelhodev <gs.coelho_dev@outlook.com>
2023-02-01 15:27:16 -0300

    feat: create swagger json config

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
+
 import { router } from "./routes";
 const app = express();
 
 const port = 8080;
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
